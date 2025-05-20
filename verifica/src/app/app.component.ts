14ca81f1acb8d64e52215ce2b1b3e7167db502fe
Biran Mergane <b.mergane2007@gmail.com>
2025-05-20 10:35:42 +0000

    Secondo punto

--- verifica/src/app/app.component.ts
+++ verifica/src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Vegetables } from './models/vegetables.models';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ThisReceiver } from '@angular/compiler';
+import { VegetableListComponent } from './vegetable-list/vegetable-list.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, VegetableListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'verifica';
+  vettInfo: Vegetables[] = []
+
+  loading!: boolean
+  o!: Observable<Vegetables[]>
+  http!: HttpClient
+
+  constructor(http: HttpClient) {this.http = http}
+
+  makeGet()
+  {
+    this.loading = true
+    this.o = this.http.get<Vegetables[]>("https://my-json-server.typicode.com/malizia-g/fine_anno_exp/verdure")
+    this.o.subscribe(this.getData)
+  }
+  getData = (dati: Vegetables[]) =>
+  {
+    this.vettInfo = dati
+    this.loading = false
+    console.log(this.vettInfo)
+  }
+  
+  ngOnInit(): void {
+    this.makeGet()
+  }
 }
---
+++ verifica/src/app/vegetable-list/vegetable-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Vegetables } from '../models/vegetables.models';
+
+@Component({
+  selector: 'app-vegetable-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './vegetable-list.component.html',
+  styleUrl: './vegetable-list.component.css'
+})
+export class VegetableListComponent {
+ @Input() verdure!: Vegetables
+}
