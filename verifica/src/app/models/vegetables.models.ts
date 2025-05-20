1b4ae391cef0c49c7ad1c1baef82fbd8b0c36eb4
Biran Mergane <b.mergane2007@gmail.com>
2025-05-20 10:25:26 +0000

    Primo punto

---
+++ verifica/src/app/models/vegetables.models.ts
@@ -0,0 +1,11 @@
+export class Vegetables{
+    nome: string;
+    descrizione: string;
+    prezzo: string;
+
+    constructor(nome: string, descrizione: string, prezzo: string){
+        this.nome = nome
+        this.descrizione = descrizione
+        this.prezzo = prezzo
+    }
+}
