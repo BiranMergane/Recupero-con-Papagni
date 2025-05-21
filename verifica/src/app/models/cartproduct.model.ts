7cadf1e560d75ad45068f1d7c025d76aad0add1c
Biran Mergane <b.mergane2007@gmail.com>
2025-05-21 12:39:35 +0000

    Quinto punto

---
+++ verifica/src/app/models/cartproduct.model.ts
@@ -0,0 +1,9 @@
+export class CartProduct{
+    nome: string;
+    nVerdure: string;
+
+    constructor(nome: string, nVerdure: string){
+        this.nome = nome
+        this.nVerdure = nVerdure
+    }
+}
--- verifica/src/app/vegetable-list/vegetable-list.component.ts
+++ verifica/src/app/vegetable-list/vegetable-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Vegetables } from '../models/vegetables.models';
 import { NgModel } from '@angular/forms';
+import { CartProduct } from '../models/cartproduct.model';
 
 @Component({
   selector: 'app-vegetable-list',
@@ -16,9 +17,13 @@ export class VegetableListComponent {
   nVerdure: ""
  }
 
-
+ vettVerdure: CartProduct[] = []
  
  salva(nVerdure: HTMLInputElement){
+  this.verdureData.nome = this.verdure.nome
+  this.verdureData.nVerdure = nVerdure.value 
 
+  this.vettVerdure.push(new CartProduct(this.verdureData.nome, this.verdureData.nVerdure))
+  console.log(this.vettVerdure)
  }
 }
